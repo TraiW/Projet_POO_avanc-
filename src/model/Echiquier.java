718e123e9dff6b7db8a4590c599db2e0273a8374
val <valentinw@hotmail.fr>
2017-02-27 09:17:00 +0100

    Ajout src/

---
+++ src/model/Echiquier.java
@@ -0,0 +1,45 @@
+package model;
+
+public class Echiquier implements Pieces {
+
+
+
+	@Override
+	public boolean capture() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Couleur getCouleur() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isMoveOk(int xFinal, int yFinal) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean move(int xFinal, int yFinal) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+
+}
