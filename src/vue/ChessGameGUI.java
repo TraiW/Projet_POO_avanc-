8a7108eaa014f8d42f575d796e4c62447f2dbb30
camblrt <camille.bleriot@cpe.fr>
2017-03-10 13:51:15 +0100

    Fin

--- src/vue/ChessGameGUI.java
+++ src/vue/ChessGameGUI.java
@@ -34,145 +34,132 @@ int yAdjustment;
 Dimension boardSize;
 ChessGameControlers chessGameControler;
 
-	public ChessGameGUI(String string, ChessGameControlers chessGameControler, Dimension dim) {
+public ChessGameGUI(String string, ChessGameControlers chessGameControler, Dimension dim) {
 		
-		boardSize = dim;
-		
-		chessGameControler=this.chessGameControler;
-		
-
-		 layeredPane = new JLayeredPane();
-		 	
-		 	//getContentPane().removeAll();
-			getContentPane().add(layeredPane);
-			
-			layeredPane.setPreferredSize(boardSize);
-			layeredPane.addMouseListener(this);
-			layeredPane.addMouseMotionListener(this); 
-			
-			chessBoard = new JPanel();
-			  layeredPane.add(chessBoard, JLayeredPane.DEFAULT_LAYER);
-			  chessBoard.setLayout( new GridLayout(8, 8) );
-			  chessBoard.setPreferredSize( boardSize );
-			  chessBoard.setBounds(0, 0, boardSize.width, boardSize.height);
-			 
-			  for (int i = 0; i < 64; i++) {
-				  JPanel square = new JPanel( new BorderLayout() );
-				  chessBoard.add( square );
-				 
-				  int row = (i / 8) % 2;
-				  if (row == 0)
-				  {
-					  square.setBackground( i % 2 == 0 ? Color.black : Color.white );
-				  }
-				  else
-				  {
-					  square.setBackground( i % 2 == 0 ? Color.white : Color.black );
-				  }
-			  }
-			  
-		
-		
+	boardSize = dim;
+	
+	chessGameControler=this.chessGameControler;
+	
+	layeredPane = new JLayeredPane();
+	 	
+	 	//getContentPane().removeAll();
+	getContentPane().add(layeredPane);
+	
+	layeredPane.setPreferredSize(boardSize);
+	layeredPane.addMouseListener(this);
+	layeredPane.addMouseMotionListener(this); 
+	
+	chessBoard = new JPanel();
+	layeredPane.add(chessBoard, JLayeredPane.DEFAULT_LAYER);
+	chessBoard.setLayout( new GridLayout(8, 8) );
+	chessBoard.setPreferredSize( boardSize );
+	chessBoard.setBounds(0, 0, boardSize.width, boardSize.height);
 		 
-	}
+	for (int i = 0; i < 64; i++) {
+		 JPanel square = new JPanel( new BorderLayout() );
+		 chessBoard.add( square );
+		 int row = (i / 8) % 2;
+		 if (row == 0)
+		 {
+			 square.setBackground( i % 2 == 0 ? Color.black : Color.white );
+		 }
+		 else
+		 {
+			 square.setBackground( i % 2 == 0 ? Color.white : Color.black );
+		 }
+  }
+
+	 
+}
 //fin constructor
 	
 	
 	
 	@Override
-	public void update(Observable arg0, Object arg1) {
+public void update(Observable arg0, Object arg1) {
 		
-		 List<PieceIHM> pieceIHM = (List<PieceIHM>)arg1;
-		 JLabel piece=null;
-		 JPanel panel=null;
-		 
-		 
-		 
-		 
-		  for(PieceIHM p : pieceIHM)
-		  {
-			  for(Coord c : p.getList())
-			  {
-				  
-				  piece = new JLabel(new ImageIcon(ChessImageProvider.getImageFile(p.getTypePiece(), p.getCouleur())));
-				  
-				  // A effacer 
-				  piece.setToolTipText("" + c.x + "," + c.y);
-				  
-				  
-				  // panel = (JPanel)chessBoard.getComponentAt(c.x,c.y);
-				  panel = (JPanel) chessBoard.getComponent(c.x + c.y *8);
-				  panel.removeAll();
-				  panel.add(piece);
-				  
-				  System.out.println(c.x * 8 + c.y + " => " + panel.getBackground());
-			  }
-		  }
-		
-		  chessBoard.revalidate();
-		  chessBoard.repaint();
-	}
+	List<PieceIHM> pieceIHM = (List<PieceIHM>)arg1;
+	JLabel piece=null;
+	JPanel panel=null;
 
+	for(PieceIHM p : pieceIHM)
+	{
+	  for(Coord c : p.getList())
+	  {
+		  piece = new JLabel(new ImageIcon(ChessImageProvider.getImageFile(p.getTypePiece(), p.getCouleur())));
+		  panel = (JPanel) chessBoard.getComponent(c.x + c.y *8);
+		  panel.removeAll();
+		  panel.add(piece);
+	  
+		  System.out.println(c.x * 8 + c.y + " => " + panel.getBackground());
+		 }
+	  }
+	
+	  chessBoard.revalidate();
+	  chessBoard.repaint();
+}
 	
 	
+public void mousePressed(MouseEvent e){
 	
-	
-	
-	
-	
-	  public void mousePressed(MouseEvent e){
-		  chessPiece = null;
-		  Component c =  chessBoard.findComponentAt(e.getX(), e.getY());
-		 
-		  if (c instanceof JPanel) 
+	  chessPiece = null;
+	  Component c =  chessBoard.findComponentAt(e.getX(), e.getY());
+	 
+	  if (c instanceof JPanel) 
 		  return;
-		 
-		  Point parentLocation = c.getParent().getLocation();
-		  xAdjustment = parentLocation.x - e.getX();
-		  yAdjustment = parentLocation.y - e.getY();
-		  chessPiece = (JLabel)c;
-		  chessPiece.setLocation(e.getX() + xAdjustment, e.getY() + yAdjustment);
-		  chessPiece.setSize(chessPiece.getWidth(), chessPiece.getHeight());
-		  layeredPane.add(chessPiece, JLayeredPane.DRAG_LAYER);
-		  }
+	 
+	  Point parentLocation = c.getParent().getLocation();
+	  xAdjustment = parentLocation.x - e.getX();
+	  yAdjustment = parentLocation.y - e.getY();
+	  chessPiece = (JLabel)c;
+	  chessPiece.setLocation(e.getX() + xAdjustment, e.getY() + yAdjustment);
+	  chessPiece.setSize(chessPiece.getWidth(), chessPiece.getHeight());
+	  layeredPane.add(chessPiece, JLayeredPane.DRAG_LAYER);
+	  
+}
 		 
 		  //Move the chess piece around
 		  
-		  public void mouseDragged(MouseEvent me) {
-		  if (chessPiece == null) return;
-		 chessPiece.setLocation(me.getX() + xAdjustment, me.getY() + yAdjustment);
-		 }
-		 
-		  //Drop the chess piece back onto the chess board
-		 
-		  public void mouseReleased(MouseEvent e) {
-		  if(chessPiece == null) return;
-		 
-		  chessPiece.setVisible(false);
-		  Component c =  chessBoard.findComponentAt(e.getX(), e.getY());
-		 
-		  if (c instanceof JLabel){
+public void mouseDragged(MouseEvent me) {
+	  if (chessPiece == null) return;
+	  	chessPiece.setLocation(me.getX() + xAdjustment, me.getY() + yAdjustment);
+	 }
+	 
+	  //Drop the chess piece back onto the chess board
+	 
+public void mouseReleased(MouseEvent e) {
+	  if(chessPiece == null) 
+		  return;
+	 
+	  chessPiece.setVisible(false);
+	  Component c =  chessBoard.findComponentAt(e.getX(), e.getY());
+	 
+	  if (c instanceof JLabel){
 		  Container parent = c.getParent();
 		  parent.remove(0);
 		  parent.add( chessPiece );
-		  }
-		  else {
+	  }
+	  else {
 		  Container parent = (Container)c;
 		  parent.add( chessPiece );
-		  }
-		 
-		  chessPiece.setVisible(true);
-		  }
-		 
-		  public void mouseClicked(MouseEvent e) {
-		  
-		  }
-		  public void mouseMoved(MouseEvent e) {
-		 }
-		  public void mouseEntered(MouseEvent e){
-		  
-		  }
-		  public void mouseExited(MouseEvent e) {
-		  
-		  }
+	  }
+	 
+	  chessPiece.setVisible(true);
+}
+	 
+
+public void mouseClicked(MouseEvent e) {
+	  
+}
+
+public void mouseMoved(MouseEvent e) {
+}
+
+public void mouseEntered(MouseEvent e){
+	  
+}
+
+public void mouseExited(MouseEvent e) {
+	  
+}
 }
